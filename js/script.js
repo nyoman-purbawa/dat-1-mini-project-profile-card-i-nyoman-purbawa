924432f98c198768957632c3161ff52ec798255f
nyoman-purbawa <inyomanpurbawa4@gmail.com>
2024-01-16 18:45:04 +0700

    Day 10 Refactoring code style dengan menggunakan Bootstrap

--- js/script.js
+++ js/script.js
@@ -6,18 +6,6 @@ document.getElementById("button").addEventListener("click", () => {
   const subject = document.getElementById("subject").value;
   const massage = document.getElementById("massage").value;
 
-  if (nama == "") {
-    alert("Nama Tidak Boleh Kosong");
-  } else if (email == "") {
-    alert("Email Tidak Boleh Kosong");
-  } else if (number == "") {
-    alert("Number Tidak Boleh Kosong");
-  } else if (subject == "") {
-    alert("Subject Tidak Boleh Kosong");
-  } else if (massage == "") {
-    alert("Massage Tidak Boleh Kosong");
-  }
-
   let a = document.createElement("a");
 
   a.href = `mailto:${email}?subject=${subject}&body=${encodeURIComponent(
